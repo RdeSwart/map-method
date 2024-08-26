9bed00f13e7eca3093628da67c26c427d5433233
RdeSwart <162818151+RdeSwart@users.noreply.github.com>
2024-08-26 16:52:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# The `map()` Method
+
+To use this repository:
+- Click the "Use this template" button in the upper right
+- Choose a name for your repository and create it
+- In your own repository, click the green "Gitpod" button
---
+++ map.js
@@ -0,0 +1,39 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node map.js in the terminal
+ */
+
+// Using a for loop
+
+
+// Using map()
+
+
+// Simplified w/ map()
+
+
+// Simplfied w/ map() + arrow function
+
+
+// With objects:
+const students = [
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skill: 'JavaScript'
+  },
+  {
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skill: 'HTML'
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skill: 'CSS'
+  },
+];
