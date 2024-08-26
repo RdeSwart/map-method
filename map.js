ccfc5b9b6d11fd07b0e92f6cf1593f51764abaf5
Ruth De Swart <ruthdeswart@outlook.ie>
2024-08-26 19:44:53 +0000

    map() method examples

--- map.js
+++ map.js
@@ -5,15 +5,28 @@
  */
 
 // Using a for loop
-
+let nums = [1,2,3,4,5];
+let results = [];
+for(let num of nums) {
+  results.push(num * 2);
+}
+console.log(results)
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
+// Simplified further w/ map()
+const simplified = nums.map(function (num) { return num * 2});
+console.log(simplified);
 
-// Simplified w/ map()
+// Simplfied even further w/ map() + arrow function
+const arrow = nums.map(num => num * 2); //turned callback function into an arrow function
+console.log(arrow);
 
-
-// Simplfied w/ map() + arrow function
 
 
 // With objects:
@@ -37,3 +50,23 @@ const students = [
     skill: 'CSS'
   },
 ];
+//returns as an array of arrays
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+// return as objects
+const objStudentWithId = students.map(elem => (
+  {
+    name: elem.name,
+    id: elem.id
+  } 
+));
+console.log(objStudentWithId);
+
+
+// None of these work!!! Return when you learn how!!!
+//Using spread syntax to add the 'age' key-value pair
+// let newObj = { ...students, age: '32', '26','28'};
+// Object.assign(students, {age: 32, age: 26, age:28});
+// console.log(students);
+
